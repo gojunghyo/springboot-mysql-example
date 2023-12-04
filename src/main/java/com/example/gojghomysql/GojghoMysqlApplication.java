7c25ecd414fec686e35af4a6ae47d0e5ddb66357
ITO_MZ_GOJGHO\mz_gojgho <gojgho@mz.co.kr>
2023-12-04 15:42:04 +0900

    first commit

---
+++ src/main/java/com/example/gojghomysql/GojghoMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.gojghomysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GojghoMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GojghoMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gojghomysql/GojghoMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gojghomysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GojghoMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
